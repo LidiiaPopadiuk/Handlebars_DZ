760852f9221b871d83e9b9b0ea00fb8d14630dc7
lidiia <lidiiadolfin@gmail.com>
2024-12-27 10:17:30 +0100

    first commit

---
+++ dist/index.1c974c2f.js
@@ -0,0 +1,60 @@
+alert("Hello, Parcel!");
+const countries = [
+    {
+        name: "\u0410\u0432\u0441\u0442\u0440\u0430\u043B\u0456\u044F",
+        capital: "\u041A\u0430\u043D\u0431\u0435\u0440\u0440\u0430",
+        currency: "\u0410\u0432\u0441\u0442\u0440\u0430\u043B\u0456\u0439\u0441\u043A\u0438\u0439 \u0434\u043E\u043B\u043B\u0430\u0440 (AUD)",
+        language: "\u0410\u043D\u0433\u043B\u0456\u0439\u0441\u043A\u0430",
+        population: 24970200,
+        flag: "https://wikiway.com/upload/iblock/742/800pxFlagofAustraliasvg.png",
+        infected: false
+    },
+    {
+        name: "\u0411\u043E\u043B\u0456\u0432\u0456\u044F",
+        capital: "\u0421\u0443\u043A\u0440\u0435",
+        currency: "\u0411\u043E\u043B\u0456\u0432\u0456\u0430\u043D\u043E (BOB)",
+        language: "\u0406\u0441\u043F\u0430\u043D\u0441\u044C\u043A\u0430, \u043A\u0435\u0447\u0443\u0430, \u0430\u0439\u043C\u0430\u0440\u0430, \u0433\u0443\u0430\u0440\u0430\u043D\u0456",
+        population: 10461053,
+        flag: "https://wikiway.com/upload/iblock/5ed/800px-Flag_of_Bolivia.png",
+        infected: true
+    },
+    {
+        name: "\u0412\u0435\u043B\u0438\u043A\u043E\u0431\u0440\u0438\u0442\u0430\u043D\u0456\u044F",
+        capital: "\u041B\u043E\u043D\u0434\u043E\u043D",
+        currency: "\u0424\u0443\u043D\u0442 \u0441\u0442\u0435\u0440\u043B\u0456\u043D\u0433\u0456\u0432 (GBP)",
+        language: "\u0410\u043D\u0433\u043B\u0438\u0439\u0441\u043A\u0430",
+        population: 63395574,
+        flag: "https://wikiway.com/upload/iblock/5ec/800px-Flag_of_the_United_Kingdom_svg.png",
+        infected: false
+    },
+    {
+        name: "\u042F\u043C\u0430\u0439\u043A\u0430",
+        capital: "\u041A\u0456\u043D\u0433\u0441\u0442\u043E\u043D",
+        currency: "\u042F\u043C\u0430\u0439\u0441\u043A\u0438\u0439 \u0434\u043E\u043B\u043B\u0430\u0440 (JMD)",
+        language: "\u0410\u043D\u0433\u043B\u0456\u0439\u0441\u044C\u043A\u0430",
+        population: 2868380,
+        flag: "https://wikiway.com/upload/iblock/b83/600px-Flag_of_Jamaica_svg.png",
+        infected: false
+    },
+    {
+        name: "\u0428\u0432\u0435\u0446\u0456\u044F",
+        capital: "\u0421\u0442\u043E\u043A\u0433\u043E\u043B\u044C\u043C",
+        currency: "\u0428\u0432\u0435\u0434\u0441\u044C\u043A\u0430 \u043A\u0440\u043E\u043D\u0430 (SEK)",
+        language: "\u0428\u0432\u0435\u0434\u0441\u044C\u043A\u0430",
+        population: 9723809,
+        flag: "https://wikiway.com/upload/iblock/5e3/800px-Flag_of_Sweden_svg.png",
+        infected: true
+    },
+    {
+        name: "\u041F\u043E\u0440\u0442\u0443\u0433\u0430\u043B\u0456\u044F",
+        capital: "\u041B\u0456\u0441\u0441\u0430\u0431\u043E\u043D",
+        currency: "\u0415\u0432\u0440\u043E (EUR)",
+        language: "\u041F\u043E\u0440\u0442\u0443\u0433\u0430\u043B\u044C\u0441\u043A\u0430",
+        population: 10799270,
+        flag: "https://wikiway.com/upload/iblock/2b1/600px-Flag_of_Portugal_svg.png",
+        infected: true
+    }
+];
+const countriesTemplate = document.querySelector('.wrapper_countries');
+
+//# sourceMappingURL=index.1c974c2f.js.map
